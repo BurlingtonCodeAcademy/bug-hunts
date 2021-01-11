b2aa80948b42f354014138d8a76a2f4647292e80
Bob <rstauss@burlingtoncodeacademy.com>
2021-01-11 13:30:57 -0500

    week 4 solution

--- week-4/index.js
+++ week-4/index.js
@@ -3,17 +3,23 @@ let display = document.getElementById('display')
 let submit = document.getElementById('submit')
 let clear = document.getElementById('clear')
 let boxes = document.querySelectorAll('li')
+let title = document.getElementById('title')
 
-title.addEventListener('hover', (event) => {
+title.addEventListener('mouseover', (event) => {
   event.target.style.color = 'red'
 })
 
-boxes.addEventListener('click', (event) => {
-  event.target.style.backgroundColor = 'blue'
+boxes.forEach(box => {
+  console.log(box)
+  box.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'blue'
+  })
 })
 
-submit.addEventListener('submit', (event) => {
-  let input = userIn.textContent
+submit.addEventListener('click', (event) => {
+  event.preventDefault()
+  let input = userIn.value
+  console.log(input)
   userIn.textContent = ''
 
   display.textContent += input
