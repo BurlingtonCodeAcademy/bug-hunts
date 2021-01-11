2a926dcfa9f79cc32539fa15046847c0224305fa
Bob <rstauss@burlingtoncodeacademy.com>
2021-01-11 14:23:21 -0500

    week 6 solution

--- week-6/app.js
+++ week-6/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path')
 const express = require('express');
 
 const app = express();
@@ -9,30 +10,31 @@ app.use(express.static('public'));
 
 //when visiting search page, send over search HTML
 app.get('/search', (request, response) => {
-  response.sendFile('./search.html')
+  response.sendFile(path.resolve('./public/search.html'))
 });
 
 
 //API endopint for Search.json
-app.get('/search', (request, response) => {
+app.get('/api/search', (request, response) => {
   let results = searchArticles(request.query)
   response.type('text/json');
   response.send(JSON.stringify(results));
 });
 
 //Create new article page
-app.get('/publish', (request, response)=>{
-  response.sendFile('./publish.html')
+app.get('/publish', (request, response) => {
+  response.sendFile(path.resolve('./public/publish.html'))
 });
 
 //when you submit the new article form
-app.post('/articles', express.urlencoded({extended: false}), (request, response)=>{
+app.post('/articles', express.urlencoded({ extended: false }), (request, response) => {
   createArticle(nextArticleId(), request.body, response)
 })
 
 //view all articles API endpoint
-app.get('/articles', (request, response) => {
+app.get('/api/articles', (request, response) => {
   let articles = allArticles();
+  console.log(articles)
   let data = JSON.stringify(articles);
   response.type('text/json');
   response.send(data);
@@ -40,24 +42,24 @@ app.get('/articles', (request, response) => {
 
 //view all articles page
 app.get('/articles', (request, response) => {
-  response.sendFile('articles.html')
+  response.sendFile(path.resolve('./public/articles.html'))
 });
 
 //view specific article API endpoint
-app.get('/articles/articleId', (request, response) => {
-  response.sendFile(request.params.articleId);
+app.get('/api/:articleId', (request, response) => {
+  response.sendFile(path.resolve('./articles/' + request.params.articleId));
 });
 
 //View specific article HTML page
-app.get('/articles/articleId', (request, response) => {
+app.get('/articles/:articleId', (request, response) => {
   const articleId = request.params.articleId;
   let filePath = articleFilePath(articleId);
   if (fs.existsSync(filePath)) {
-    let htmlFile = path.join("article.html");
+    let htmlFile = path.resolve("./public/article.html");
     response.sendFile(htmlFile);
   }
   else {
-    response.sendStatus(404, `Article ${params.id} not found...`)
+    response.sendStatus(404, `Article ${request.params.id} not found...`)
   }
 });
 
@@ -68,19 +70,20 @@ app.listen(port, () => {
 
 //Helper functions
 function allArticles() {
+  let articlesDir = path.resolve('./articles')
   return fs.readdirSync(articlesDir)
-  .filter(file => file.endsWith('.json'))
-  .map(file => JSON.parse(fs.readFileSync(path.join(articlesDir, file))))
-  .sort((a,b)=> (a.id - b.id));
+    .filter(file => file.endsWith('.json'))
+    .map(file => JSON.parse(fs.readFileSync(path.join(articlesDir, file))))
+    .sort((a, b) => (a.id - b.id));
 }
 
 function articleFilePath(articleId) {
-  return path.join(articlesDir, articleId + '.json');
+  return path.resolve('./articles/' + articleId + '.json');
 }
 
 function searchArticles(params) {
   let results = allArticles().filter((article) => {
-    if(params.author) {
+    if (params.author) {
       let articleAuthor = article.author || '';
       let targetAuthor = params.author || '';
       return articleAuthor.toLowerCase().includes(targetAuthor.toLowerCase());
