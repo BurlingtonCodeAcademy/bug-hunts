303c778ebe6a51e54f76feda3fed6b1e0cd719d2
Bob <rstauss@burlingtoncodeacademy.com>
2021-01-29 09:26:03 -0500

    better solution

--- week-1/add-em-up.js
+++ week-1/add-em-up.js
@@ -1,37 +1,40 @@
 //Boilerplate code set up correctly. No bugs here!
-const readline = require('readline')
-let interface = readline.createInterface(process.stdin, process.stdout)
+const readline = require("readline");
+let interface = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
-    interface.question(questionText, resolve)
-  })
+    interface.question(questionText, resolve);
+  });
 }
 
 //Beware! Beyond this point lie bugs!
 async function adder() {
-  let response = await ask("Let's add some numbers together!\nAre you ready? ")
+  let response = await ask("Let's add some numbers together!\nAre you ready? ");
 
-  while (response !== 'yes') {
-    response = await ask('Are you ready now? ')
+  while (response !== "yes") {
+    response = await ask("Are you ready now? ");
   }
 
-  let number = await ask('What number are we starting with? ')
+  let number = await ask("What number are we starting with? ");
 
-  if(parseInt(number) === NaN) {
-    number = await ask("Let's try this again. Please enter a number. ")
-  } else {
-    let addThis = await ask("Keep entering numbers to add. Then type 'done' to see the result. ")
-
-    while (addThis !== 'done') {
-      number = parseInt(number)
-      number += parseInt(addThis)
-      
-      addThis = await ask('Enter the next number, or "done" ')
-    }
-
-    console.log(number)
+  while (isNaN(number)) {
+    number = await ask("Let's try this again. Please enter a number. ");
   }
+  let addThis = await ask(
+    "Keep entering numbers to add. Then type 'done' to see the result. "
+  );
+
+  while (addThis !== "done") {
+    number = parseInt(number);
+    number += parseInt(addThis);
+
+    addThis = await ask('Enter the next number, or "done" ');
+  }
+
+  console.log(number);
+
+  process.exit();
 }
 
-adder()
+adder();
